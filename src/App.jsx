a4886556b42440885172356a03aa4a25a9d81b9d
Leonardo Zambão <leonardozambao@live.com>
2026-01-16 19:06:16 -0300

    components footer + navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Footer from "./layout/Footer";
 import Home from "./pages/home/Home";
 import About from "./pages/about/About";
-
+import Navbar from "./components/Navbar";
 function App() {
   return (
     <>
       <main>
         <BrowserRouter>
+          <Navbar />
           <div className="container">
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/about" element={<About />} />
             </Routes>
           </div>
+          <Footer />
         </BrowserRouter>
       </main>
     </>
