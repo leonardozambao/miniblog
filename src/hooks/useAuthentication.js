28d97329c43faa35cbab343613ca7a9a5650ff36
Leonardo Zambão <leonardozambao@live.com>
2026-01-16 21:00:02 -0300

    register user firebase

---
+++ src/hooks/useAuthentication.js
@@ -0,0 +1,116 @@
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  updateProfile,
+  signOut,
+} from "firebase/auth";
+
+import { db } from "../firebase/config";
+import { useState, useEffect } from "react";
+
+export const useAuthentication = () => {
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(null);
+
+  // deal with memory leak
+  const [cancelled, setCancelled] = useState(false);
+
+  const auth = getAuth();
+
+  function checkIfIsCancelled() {
+    if (cancelled) {
+      return;
+    }
+  }
+
+  const createUser = async (data) => {
+    checkIfIsCancelled();
+
+    setLoading(true);
+    setError(null);
+
+    try {
+      const { user } = await createUserWithEmailAndPassword(
+        auth,
+        data.email,
+        data.password,
+      );
+
+      await updateProfile(user, {
+        displayName: data.displayName,
+      });
+      setLoading(false);
+
+      return user;
+    } catch (error) {
+      console.log(error.message);
+      console.log(typeof error.message);
+
+      let systemErrorMessage;
+
+      if (error.message.includes("Password")) {
+        systemErrorMessage = "Password must contain at least 6 characters.";
+      } else if (error.message.includes("email-already")) {
+        systemErrorMessage = "Email already registered.";
+      } else {
+        systemErrorMessage = "An error occurred, please try again later.";
+      }
+      setLoading(false);
+
+      setError(systemErrorMessage);
+    }
+  };
+
+  const logout = () => {
+    checkIfIsCancelled();
+
+    signOut(auth);
+  };
+
+  const login = async (data) => {
+    checkIfIsCancelled();
+
+    setLoading(true);
+    setError(false);
+
+    try {
+      await signInWithEmailAndPassword(auth, data.email, data.password);
+    } catch (error) {
+      console.log(error.message);
+      console.log(typeof error.message);
+      console.log(error.message.includes("user-not"));
+
+      let systemErrorMessage;
+
+      if (error.message.includes("user-not-found")) {
+        systemErrorMessage = "Usuário não encontrado.";
+      } else if (error.message.includes("wrong-password")) {
+        systemErrorMessage = "Senha incorreta.";
+      } else {
+        systemErrorMessage = "Ocorreu um erro, por favor tenta mais tarde.";
+      }
+
+      console.log(systemErrorMessage);
+
+      setError(systemErrorMessage);
+    }
+
+    console.log(error);
+
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    return () => setCancelled(true);
+  }, []);
+
+  return {
+    auth,
+    createUser,
+    error,
+    logout,
+    login,
+    loading,
+  };
+};
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,14 +1,48 @@
 import styles from "./Register.module.css";
 import { useState, useEffect } from "react";
+import { useAuthentication } from "../../hooks/useAuthentication";
 
 const Register = () => {
+  const [displayName, setDisplayName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const { createUser, error: authError, loading } = useAuthentication();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+    const user = {
+      displayName,
+      email,
+      password,
+    };
+    if (password !== confirmPassword) {
+      setError("Passwords do not match");
+      alert("Passwords do not match");
+      return;
+    }
+
+    const res = await createUser(user);
+
+    console.table(res);
+  };
+
+  useEffect(() => {
+    if (authError) {
+      setError(authError);
+    }
+  }, [authError]);
+
   return (
     <div className={styles.register}>
       <h1>Register to post blogs</h1>
       <p>
         Already have an account? <a href="/login">Login</a>
       </p>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
           <span>Name:</span>
           <input
@@ -16,6 +50,8 @@ const Register = () => {
             name="displayName"
             required
             placeholder="Enter your name"
+            value={displayName}
+            onChange={(e) => setDisplayName(e.target.value)}
           />
         </label>
         <label>
@@ -25,6 +61,8 @@ const Register = () => {
             name="email"
             required
             placeholder="Enter your email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
         </label>
         <label>
@@ -34,6 +72,8 @@ const Register = () => {
             name="password"
             required
             placeholder="Enter your password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
         </label>
         <label>
@@ -43,11 +83,21 @@ const Register = () => {
             name="confirmPassword"
             required
             placeholder="Confirm your password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
           />
         </label>
-        <button type="submit" className="button">
-          Register
-        </button>
+        {!loading && (
+          <button type="submit" className="button">
+            Register
+          </button>
+        )}
+        {loading && (
+          <button type="submit" className="button" disabled>
+            Registering...
+          </button>
+        )}
+        {error && <p className="error">{error}</p>}
       </form>
     </div>
   );
