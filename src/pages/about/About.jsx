c78c641d0db55af35c1a2568be437ba6de310b0a
Leonardo Zambão <leonardozambao@live.com>
2026-02-02 10:58:27 -0300

    about page

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,10 +1,21 @@
 import styles from "./About.module.css";
 
+import { Link } from "react-router-dom";
+
 const About = () => {
   return (
-    <>
-      <h1>About</h1>
-    </>
+    <div className={styles.about}>
+      <h2>
+        About the Mini <span>Blog</span>
+      </h2>
+      <p>
+        This project consists of a blog made with React on the front-end and Firebase
+        on the back-end.
+      </p>
+      <Link to="/posts/create" className="button">
+        Create post
+      </Link>
+    </div>
   );
 };
 
