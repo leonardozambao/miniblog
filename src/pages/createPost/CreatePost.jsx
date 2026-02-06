340e7e5a38ea74fde936e26be6866e6ea9f97463
Leonardo Zambão <leonardozambao@live.com>
2026-02-06 10:24:38 -0300

    create post

--- src/pages/createPost/CreatePost.jsx
+++ src/pages/createPost/CreatePost.jsx
@@ -1,11 +1,119 @@
 import styles from "./CreatePost.module.css";
 
 import { useState } from "react";
+import { useInsertDocument } from "../../hooks/useInsertDocument";
+import { useNavigate } from "react-router-dom";
+import { useAuthValue } from "../../context/AuthContext";
 
 const CreatePost = () => {
-    return (
-        <div className={styles.createPost}>CreatePost</div>
-    )
-}
+    const [title, setTitle] = useState("");
+    const [image, setImage] = useState("");
+    const [body, setBody] = useState("");
+    const [tags, setTags] = useState([]);
+    const [formError, setFormError] = useState("");
 
-export default CreatePost
+    const { user } = useAuthValue();
+
+    const navigate = useNavigate();
+
+    const { insertDocument, response } = useInsertDocument("posts");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setFormError("");
+
+        // validate image
+        try {
+            new URL(image);
+        } catch (error) {
+            setFormError("The image must be a URL.");
+            return;
+        }
+
+        // create tags array
+        const tagsArray = tags.split(",").map((tag) => tag.trim().toLowerCase());
+
+        // check values
+        if (!title || !image || !tags || !body) {
+            setFormError("Please fill in all fields!");
+            return;
+        }
+
+        if (formError) return;
+
+        insertDocument({
+            title,
+            image,
+            body,
+            tags: tagsArray,
+            uid: user.uid,
+            createdBy: user.displayName,
+        });
+
+        // redirect to home page
+        navigate("/");
+    };
+
+    return (
+        <div className={styles.createPost}>
+            <h2>Create post</h2>
+            <p>Write about whatever you want and share your knowledge!</p>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    <span>Title:</span>
+                    <input
+                        type="text"
+                        name="text"
+                        required
+                        placeholder="Think of a good title..."
+                        onChange={(e) => setTitle(e.target.value)}
+                        value={title}
+                    />
+                </label>
+                <label>
+                    <span>Image URL:</span>
+                    <input
+                        type="text"
+                        name="image"
+                        required
+                        placeholder="Insert an image that represents your post"
+                        onChange={(e) => setImage(e.target.value)}
+                        value={image}
+                    />
+                </label>
+                <label>
+                    <span>Content:</span>
+                    <textarea
+                        name="body"
+                        required
+                        placeholder="Insert the post content"
+                        onChange={(e) => setBody(e.target.value)}
+                        value={body}
+                    ></textarea>
+                </label>
+                <label>
+                    <span>Tags:</span>
+                    <input
+                        type="text"
+                        name="tags"
+                        required
+                        placeholder="Insert tags separated by commas"
+                        onChange={(e) => setTags(e.target.value)}
+                        value={tags}
+                    />
+                </label>
+                {!response.loading && <button className="button">Create post!</button>}
+                {response.loading && (
+                    <button className="button" disabled>
+                        Wait...
+                    </button>
+                )}
+                {(response.error || formError) && (
+                    <p className="error">{response.error || formError}</p>
+                )}
+            </form>
+        </div>
+    );
+};
+
+export default CreatePost;
